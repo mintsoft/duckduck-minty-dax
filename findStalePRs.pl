18b6598bd87c650b025cd2590fcaa8880650cebc
Rob <rob@mintsoft.net>
2017-08-24 23:36:08 +0100

    Simplifying, in reality I want to see the last response and who from each and every PR

--- findStalePRs.pl
+++ findStalePRs.pl
@@ -7,33 +7,38 @@ use JSON::XS;
 use List::Util qw(first max maxstr min minstr reduce shuffle sum any);
 use File::Slurp qw(read_file write_file);
 
-my $meta = decode_json(read_file('meta/repo_all.json'));
-
-my ($token) = @ARGV;
-
 my $browser = LWP::UserAgent->new;
-$browser->default_header("Authorization" => "token $token");
+#$browser->default_header("Authorization" => "token $token");
 
-for my $page (0..1) {
-    $json = $browser->get("https://api.github.com/repos/duckduckgo/zeroclickinfo-goodies/issues?labels=Maintainer%20Input%20Requested");
+for my $page (1..1) {
+    $json = $browser->get("https://api.github.com/repos/duckduckgo/zeroclickinfo-goodies/issues?page=${page}&per_page=100");
 	$issues = decode_json($json->content);
 	foreach my $issue(@{$issues}){
 		next unless defined $issue->{'pull_request'};
 		
         my $comment_response = $browser->get($issue->{'comments_url'});
         my $comments = decode_json($comment_response->content);
-
+	my $updated_at = $issue->{'updated_at'};
         my $pr_desc = $comments->[0]->{'body'};
+	print dump($comments);
+	my $latest_comment = {
+		created_at => '1970-01-01T00:00:00Z',
+		updated_at => '1970-01-01T00:00:00Z',
+		user => "",
+		message => "",
+	};
+	for my $comment ( @{$comments} ) {
+		if($latest_comment->{'created_at'} lt $comment->{'created_at'}) {
+			$comment = {
+				created_at => $comment->{'created_at'},
+				updated_at => $comment->{'updated_at'},
+				user => $comment->{'user'}->{'login'},
+				message => $comment->{'body'},
+			};
+		}
+	}
 
-        my ($maintainer, $ia_page) = get_maintainer($pr_desc);
-        my $maintainer_replied = any { lc($_->{'user'}->{'login'}) eq lc($maintainer) } @{$comments};
-        print "$issue->{'number'}\t$issue->{'created_at'}\t$maintainer\t$ia_page\t". ($maintainer_replied?"1":"0").$/;
+        print "$issue->{'number'}\t$issue->{'created_at'}\t$ia_page\t$latest_comment->{'updated_at'} by $latest_comment->{'user'}\n";
     }
 }
 
-sub get_maintainer {
-    my($input) = @_;
-    my ($ia_page) = $input =~ qr#https://duck.co/ia/view/([a-zA-Z0-9_-]+)#;
-    my $ia_meta = $meta->{$ia_page};
-    return $meta->{$ia_page}->{'maintainer'}->{'github'}, $ia_page;
-}
