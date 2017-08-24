10ae4dd13f0b2d08ad787282c73f556a352d65b9
Rob <rob@mintsoft.net>
2017-08-24 23:51:27 +0100

    Helps if you update the right variable when hunting for comments

--- findStalePRs.pl
+++ findStalePRs.pl
@@ -7,8 +7,10 @@ use JSON::XS;
 use List::Util qw(first max maxstr min minstr reduce shuffle sum any);
 use File::Slurp qw(read_file write_file);
 
+my ($token) = @ARGV;
+
 my $browser = LWP::UserAgent->new;
-#$browser->default_header("Authorization" => "token $token");
+$browser->default_header("Authorization" => "token $token");
 
 for my $page (1..1) {
     $json = $browser->get("https://api.github.com/repos/duckduckgo/zeroclickinfo-goodies/issues?page=${page}&per_page=100");
@@ -20,7 +22,7 @@ for my $page (1..1) {
         my $comments = decode_json($comment_response->content);
 	my $updated_at = $issue->{'updated_at'};
         my $pr_desc = $comments->[0]->{'body'};
-	print dump($comments);
+	#print dump($comments);
 	my $latest_comment = {
 		created_at => '1970-01-01T00:00:00Z',
 		updated_at => '1970-01-01T00:00:00Z',
@@ -29,7 +31,7 @@ for my $page (1..1) {
 	};
 	for my $comment ( @{$comments} ) {
 		if($latest_comment->{'created_at'} lt $comment->{'created_at'}) {
-			$comment = {
+			$latest_comment = {
 				created_at => $comment->{'created_at'},
 				updated_at => $comment->{'updated_at'},
 				user => $comment->{'user'}->{'login'},
