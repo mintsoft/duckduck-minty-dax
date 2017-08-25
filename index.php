6d8fa99648ef3d6db314b6b3bc3f2db496ce7287
Rob <rob@mintsoft.net>
2017-08-25 22:51:24 +0100

    Need user agent!

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
 
-function downloadJson($url) {
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$response = curl_exec($ch);
-	curl_close($ch);
+function curl_get($url, array $get = NULL, array $options = array())
+{   
+    $defaults = array(
+        CURLOPT_URL => $url,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_TIMEOUT => 4,
+        CURLOPT_USERAGENT => "DuckDuck-Minty-Dax"
+    );
+   
+    $ch = curl_init();
+    curl_setopt_array($ch, ($options + $defaults));
 
-	return json_decode($response, true);
-}
+    if( ! $result = curl_exec($ch))
+    {
+        trigger_error(curl_error($ch));
+    }
 
+    curl_close($ch);
+    return json_decode($result, true);
+} 
 
 ?>
 <!DOCTYPE html>
@@ -18,16 +29,21 @@ function downloadJson($url) {
 </head>
 <body>
  
-
+<table>
+<thead>
+</thead>
+<tbody>
 
 <?php
-$issues = downloadJson("https://api.github.com/repos/duckduckgo/zeroclickinfo-goodies/issues?page=1&per_page=100");
+$issues = curl_get("https://api.github.com/repos/duckduckgo/zeroclickinfo-goodies/issues?page=1&per_page=100");
+var_dump($issues);
 
+exit;
 foreach($issues as $key => $i) {
 	if(!$i["pull_request"])
 		continue;
 
-	$comments = downloadJson($i["comments_url"]);
+#	$comments = curl_get($i["comments_url"]);
 	$author = $i["user"]["login"];
 	$title = $i["title"];
 
@@ -44,5 +60,7 @@ foreach($issues as $key => $i) {
 
 }
 ?>
+</tbody>
+</table>
 </body>
 </html>
